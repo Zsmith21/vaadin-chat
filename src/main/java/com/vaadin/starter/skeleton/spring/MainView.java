cd7df41eababbd815f278edb66b567e5860794cc
Marcus Hellberg <marcus@vaadin.com>
2018-09-03 07:59:01 -0700

    remove some unused functionality to make code easier

--- src/main/java/com/vaadin/starter/skeleton/spring/MainView.java
+++ src/main/java/com/vaadin/starter/skeleton/spring/MainView.java
@@ -11,24 +11,11 @@ import com.vaadin.flow.server.PageConfigurator;
 @Route("")
 @StyleSheet("styles/styles.css")
 @JavaScript("src/ios-keyboard-fix.js")
-public class MainView extends VerticalLayout implements PageConfigurator {
+public class MainView extends VerticalLayout {
 
 
   public MainView() {
     add(new H1("Hello world!"));
-  }
-
-  //-------------------------------------------------------
-  // Temporary solution for adding Web App Manifest and ServiceWorker, will be
-  // handled by Vaadin shortly
-  @Override
-  public void configurePage(InitialPageSettings settings) {
-    settings.addLink(InitialPageSettings.Position.PREPEND, "manifest", "manifest.webmanifest");
-    settings.addLink(InitialPageSettings.Position.PREPEND, "icon", "favicon.png");
-    settings.addInlineWithContents(
-        "window.addEventListener('load', function() { " + "  if('serviceWorker' in navigator) {"
-            + "    navigator.serviceWorker.register('./sw.js');" + "  }" + "});",
-        InitialPageSettings.WrapMode.JAVASCRIPT);
   }
 
 }
